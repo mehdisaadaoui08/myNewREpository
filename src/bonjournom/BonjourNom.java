cf0faeceac8bc8ebc43db9232dced2c2660f9808
mehdi <mehdi@172.29.84.233>
2024-05-09 19:10:53 -0400

    1er commit

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom;
+
+/**
+ *
+ * @author mehdi
+ */
+import aleatoire.Person;
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x ; 
+        System.out.println(" Hello world ");
+        
+    }
+    
+  
+}
